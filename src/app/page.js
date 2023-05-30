1651cc3711c710d886bbd9ffebfa7ba2d43595d5
lilou <lidou34@live.fe>
2023-05-30 18:29:08 +0200

    ajout texte descriptig agence

--- src/app/page.js
+++ src/app/page.js
@@ -35,28 +35,19 @@ export default function page() {
       <div className={css.main__content}>
         <div ref={parentRef}>
           <div className={css.main__text} ref={textRef}>
-            <h1>Lorem ipsum </h1>
-            <h3>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-              facilisis volutpat sapien. Etiam ullamcorper ex a diam venenatis
-              fringilla. Morbi et sollicitudin sem. Vestibulum laoreet ex a leo
-              ornare malesuada.
-              <br /> Ut porta gravida libero non porttitor. Aenean in diam ut
-              quam tristique bibendum. Nunc eu fermentum nisi. Etiam purus arcu,
-              accumsan tempor elit ac, iaculis euismod dolor. Ut enim ipsum,
-              tincidunt eu risus vel, tempor convallis massa. Curabitur
-              hendrerit, risus at vulputate malesuada, velit urna tempus sem,
-              vel sagittis mauris tortor quis justo. Pellentesque varius leo
-              mauris, ac rutrum felis scelerisque a.
-              <br /> Donec at magna quis massa venenatis placerat vitae at
-              lacus. Quisque interdum ex eu erat lobortis, dignissim suscipit
-              orci luctus. In dictum eros non ornare gravida. Mauris id commodo
-              leo. Fusce varius non lectus non fringilla. Aliquam magna enim,
-              dapibus id placerat sit amet, congue eu dui. In eget nulla maximus
-              massa egestas viverra ac et ligula. Vestibulum imperdiet, massa id
-              accumsan ultricies, neque urna dictum erat, eget imperdiet enim
-              tortor id purus. Vestibulum ac venenatis arcu.
-            </h3>
+            
+            <h2>
+              Vous avez besoin de créer ou améliorer votre présence digitale, vous êtes au bon endroit !
+            </h2>
+            <h3>Pourquoi ?</h3>
+            <p>Nous avons été à votre place, malgré une forte volonté de développer sa présence digitale il faut avoir le temps, les connaissances, l'envie etc… c’est un métier, on ne s’improvise pas développeur web ou spécialiste des réseaux sociaux.</p>
+            <h3>Pour Qui ?</h3>
+            <p>Nos offres s'adressent aux professionnels, notre clientèle est surtout composée d'artisans, commerçants, petites ou moyennes entreprises, mais aussi nous accompagnons les associations ainsi que les particuliers / famille qui ont besoin d'une vitrine pour un évènement (mariage, cousinades)
+            </p>
+            <h3>Quoi ?</h3>
+            <p>Nos "codons" nos sites entièrement, ce qui évite d'alourdir votre serveur et permet une optimisation SEO. Pour les créations ou refonte de boutiques e-commerce avec un catalogue important nous privilégions Prestashop car c'est le CMS le plus libre, il est gratuit et un catalogue de modules gratuit et payant permet de vous apporter un confort de travail, il a fait ses preuves et il est considéré comme le meilleur en la matière. En parlant de module prestashop, pour les gros catalogues ou éviter les tches chronophages nous sommes partenaires Store Commander, programme qui permet de gérer votre catalogue produit, clients, commandes à la volée
+            </p>
+            
           </div>
         </div>
 
