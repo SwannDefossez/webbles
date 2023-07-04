39150954b12d1d039398a820944937c300407d6e
Swann <swann.defossezanceaux@gmail.com>
2023-07-04 14:29:04 +0200

    Accessibilité Tarif , supression image  , favicon

--- src/app/lexique/page.js
+++ src/app/lexique/page.js
@@ -25,10 +25,10 @@ export default function page() {
             site web accessible est un site qui permet aux personnes en
             situation de handicap d'accéder à ses contenus et ses
             fonctionnalités sans difficulté. Un site accessible permet par
-            exemple de : <ul><li></li> naviguer avec une synthèse vocale et/ou une plage
+            exemple de : <ul><li> naviguer avec une synthèse vocale et/ou une plage
             braille (notamment utilisées par les personnes aveugles et
             malvoyantes) ; </li>• personnaliser l’affichage du site selon ses besoins
-            (grossissement des caractères, modification des couleurs, etc.) ; •
+            (grossissement des caractères, modification des couleurs, etc.) ;</li> •
             naviguer sans utiliser la souris (avec le clavier uniquement, via un
             écran tactile, à la voix ou tout autre périphérique adapté).</ul> <p>Pour
             cela, le site doit respecter les normes en vigueur lors de sa
